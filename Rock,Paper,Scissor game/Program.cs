514be48e3d51e0ca52e070180b455cad7e0d11c2
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-06-15 16:08:44 +0200

    I have added the method used to compare the users input with the computers.I also allowed for the user to exit and restart the game.

--- Rock,Paper,Scissor game/Program.cs
+++ Rock,Paper,Scissor game/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,49 @@ namespace Rock_Paper_Scissor_game
     {
         static void Main(string[] args)
         {
+
+            bool Playing = true;
+            do
+            {
+                Console.Write("Choose Rock/Paper/Scissors: ");
+                String userChoice = Console.ReadLine().ToUpper();
+
+                String[] ComChoice = { "ROCK", "PAPER", "SCISSORS" };
+                Random random = new Random();
+                int Comrand = random.Next(ComChoice.Length);
+                String Com = ComChoice[Comrand];
+                
+
+                Console.WriteLine(CalculateResult(userChoice, Com));
+                Playing = false;
+                Console.WriteLine("Do you want to play again?(Yes/No)");
+                String replay=Console.ReadLine();
+                if (replay == "Yes" || replay == "yes") { Playing = true; }
+                else { Playing = false; Environment.Exit(0); }
+            } while (Playing == true);
+
+            Console.ReadKey();  
+        }static public String CalculateResult(String userChoice,String CompChoice)
+        {
+            String result ="Error";
+            if (userChoice =="ROCK"&& CompChoice=="ROCK"|| userChoice == "PAPER" && CompChoice == "PAPER" || userChoice == "SCISSORS" && CompChoice == "SCISSORS")
+            {
+                result = "Draw";
+                
+            }
+            if (userChoice == "ROCK" && CompChoice == "SCISSORS" || userChoice == "PAPER" && CompChoice == "ROCK" || userChoice == "SCISSORS" && CompChoice == "PAPER")
+            {
+                result = "User Wins!";
+               
+
+            }
+            if (userChoice == "SCISSORS" && CompChoice == "ROCK" || userChoice == "ROCK" && CompChoice == "PAPER" || userChoice == "PAPER" && CompChoice == "SCISSORS")
+            {
+                result = "COM Wins!You loose";
+                
+
+            }
+            return result;
         }
     }
 }
